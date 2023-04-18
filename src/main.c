9eba6a75e6c67a43b359e2fba0f6d366bdf2ea8f
Jersey <k.aubindery@gmail.com>
2023-04-18 04:13:45 +0000

    Switched to C.

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("GBA Controller\n");
+
+    return 0;
+}
--- src/main.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-
-    std::cout << "GBA Controller\n";
-
-    return 0;
-}
